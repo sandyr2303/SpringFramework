25a0a2a488c90d45886d5eb2b59ac8e9b7640aaa
sandeep kumar reddy bollapu <sandeepkumarreddybollapu@sandeeps-MBP.lan>
2024-01-21 18:34:32 -0600

    new game1

--- learn-spring-framework/src/main/java/com/sandeep/learnspringframework/AppGamingBasic.java
+++ learn-spring-framework/src/main/java/com/sandeep/learnspringframework/AppGamingBasic.java
@@ -1,15 +1,18 @@
 package com.sandeep.learnspringframework;
 
 import com.sandeep.learnspringframework.game.GameRunner;
-import com.sandeep.learnspringframework.game.MarioGame;
+//import com.sandeep.learnspringframework.game.MarioGame;
+import com.sandeep.learnspringframework.game.SuperMario;
 
 public class AppGamingBasic {
 
 	public static void main(String[] args) {
 		
-		var marioGame = new MarioGame();
+//		var marioGame = new MarioGame();
 		
-		var gameRunner = new GameRunner(marioGame);
+		var superMario = new SuperMario();
+		
+		var gameRunner = new GameRunner(superMario);
 		
 		gameRunner.run();
 
